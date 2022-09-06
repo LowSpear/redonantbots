47d6d263c0c087f52c71b6eee2ef7c37b2ff9e5c
LowSpear <ismail.navruz9@gmail.com>
2022-09-06 22:59:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const PogyClient = require("./Pogy");
 const config = require("./config.json");
 const logger = require("./src/utils/logger");
 const Pogy = new PogyClient(config);
-var http = require('http');
 
-http.createServer(function (req, res){
-  res.write("Botunuz Guvenli Bir Sekilde 7/24 Olmustur");
-  res.end();
-}).listen(8080);
 
 const color = require("./src/data/colors");
 Pogy.color = color;
